e3aec746f4a6863a8fc33d700023e7312e3baba2
Александр <learningenglish2804@gmail.com>
2023-05-28 18:13:31 +0400

    add h_w_5

--- src/playlist.py
+++ src/playlist.py
@@ -1,6 +1,6 @@
-# This Python file uses the following encoding: utf-8
-import json
+import datetime
 import os
+import isodate
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('API_KEY')
@@ -9,27 +9,55 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class PlayList:
-    def __init__(self, playlist_id):
-        """Экземпляр инициализируется id плейлиста. Дальше все данные будут подтягиваться по API."""
-        self.playlist_id = playlist_id
-        self.title = ''
-        self.url = ''
 
-    def get_service_playlist(self):
-        """Заполняет атрибуты экземпляра данными из YouTube API."""
+    def __init__(self, playlist_id: str) -> None:
+        """Инициализируем сначала ID и URL плейлиста, затем его название"""
+        self.id = playlist_id
+        self.url = f"https://www.youtube.com/playlist?list={playlist_id}"
 
-        response = youtube.playlistItems().list(playlistId=self.playlist_id,
-                                                part='contentDetails',
-                                                maxResults=50,
-                                                ).execute()
+        self.playlists_info = youtube.playlists().list(id=self.id,
+                                                       part='contentDetails,snippet',
+                                                       maxResults=50,
+                                                       ).execute()
 
-        # Заполняем атрибуты экземпляра данными о канале
-        #self.title = response['items']['snippet']['title']
-        #self.url = f"https://www.youtube.com/playlist/{self.playlist_id}"
-        #return response
+        self.videos_info = youtube.playlistItems().list(playlistId=self.id,
+                                                        part='contentDetails',
+                                                        maxResults=50,
+                                                        ).execute()
 
+        self.videos_id: list[str] = [video['contentDetails']['videoId'] for video in self.videos_info['items']]
 
-# if __name__ == '__main__':
-#     pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-#
-#     print(json.dumps(pl.get_service_playlist()))
+        self.video_response = youtube.videos().list(part='contentDetails, statistics',
+                                                    id=','.join(self.videos_id)
+                                                    ).execute()
+
+        self.title = self.playlists_info['items'][0]['snippet']['title']
+
+    @property
+    def total_duration(self):
+        """Выводим общую длительность плейлиста"""
+        total_duration = datetime.timedelta()
+        video_response = self.video_response
+
+        for video in video_response['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration1 = isodate.parse_duration(iso_8601_duration)
+            total_duration += duration1
+
+        return total_duration
+
+    def show_best_video(self):
+        """Вывод видео с наибольшим количеством лайков"""
+        most_likes = 0
+        count = int(len(self.video_response['items']))
+        most_likes_video = None
+
+        for i in range(count):
+            like_count = int(self.video_response['items'][i]['statistics']['likeCount'])
+
+            if like_count > most_likes:
+                most_likes = like_count
+                most_likes_video = self.video_response['items'][i]['id']
+
+        return f"https://youtu.be/{most_likes_video}"
+
