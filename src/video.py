ac00ea91c98aaa74f637bae6efa9d4710f81c230
Александр <learningenglish2804@gmail.com>
2023-06-04 18:33:54 +0400

    add h_w_6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,5 @@
 # This Python file uses the following encoding: utf-8
-
+import requests
 import os
 from googleapiclient.discovery import build
 
@@ -13,22 +13,33 @@ class Video:
     def __init__(self, video_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        self.video_title = ''
-        self.video_url = ''
-        self.view_count = 0
-        self.like_count = 0
+        self.video_title = None
+        self.video_url = None
+        self.view_count = None
+        self.like_count = None
 
     def get_service_video(self):
         """Заполняет атрибуты экземпляра данными из YouTube API."""
+        try:
+            checker_url = "https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v="
+            video_url = checker_url + self.video_id
 
-        response = youtube.videos().list(part='snippet, statistics',
-                                         id=self.video_id).execute()
+            request = requests.get(video_url)
 
-        # Заполняем атрибуты экземпляра данными о канале
-        self.video_title = response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/video/{self.video_id}"
-        self.view_count = response['items'][0]['statistics']['viewCount']
-        self.like_count = response['items'][0]['statistics']['likeCount']
+            if request.status_code != 200:
+                raise Exception("Такого видео нет")
+        except Exception:
+            raise Exception
+
+        else:
+            response = youtube.videos().list(part='snippet, statistics',
+                                             id=self.video_id).execute()
+
+            # Заполняем атрибуты экземпляра данными о канале
+            self.video_title = response['items'][0]['snippet']['title']
+            self.video_url = f"https://www.youtube.com/video/{self.video_id}"
+            self.view_count = response['items'][0]['statistics']['viewCount']
+            self.like_count = response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         """Возвращает строку с названием видео."""
