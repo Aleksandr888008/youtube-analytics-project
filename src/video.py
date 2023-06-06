596552d549564be1429919e4511f7174f952b5a3
Александр <learningenglish2804@gmail.com>
2023-06-06 18:54:36 +0400

    add h_w_6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
-import requests
+
+
 import os
 from googleapiclient.discovery import build
 
@@ -17,29 +18,22 @@ class Video:
         self.video_url = None
         self.view_count = None
         self.like_count = None
+        self.get_service_video()
 
     def get_service_video(self):
         """Заполняет атрибуты экземпляра данными из YouTube API."""
         try:
             response = youtube.videos().list(part='snippet, statistics',
                                              id=self.video_id).execute()
-            # checker_url = "https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v="
-            # video_url = checker_url + self.video_id
-            #
-            # request = requests.get(video_url)
-            #
-            # if request.status_code != 200:
-        except IndexError:
-            raise IndexError("Видео с таким 'video id' нет")
-
-        else:
-
 
             # Заполняем атрибуты экземпляра данными о канале
             self.video_title = response['items'][0]['snippet']['title']
             self.video_url = f"https://www.youtube.com/video/{self.video_id}"
             self.view_count = response['items'][0]['statistics']['viewCount']
             self.like_count = response['items'][0]['statistics']['likeCount']
+
+        except IndexError:
+            raise IndexError("Видео с таким 'video id' нет")
 
     def __str__(self):
         """Возвращает строку с названием видео."""
@@ -60,3 +54,9 @@ class PLVideo(Video):
         self.view_count = 0
         self.like_count = 0
         self.playlist_id = playlist_id
+
+# if __name__ == '__main__':
+#     broken_video_1 = Video('broken_video_id')
+#     #broken_video = Video('9lO06Zxhu88')
+#     #print(broken_video.video_title)
+#     print(broken_video_1.video_title)
